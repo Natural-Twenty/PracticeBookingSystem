49db3af49b10537c2eb4848444c6d6e4118ae052
Natural-Twenty <z5075710@cse.unsw.edu.au>
2020-06-22 18:47:52 +1000

    Attribute and method skeleton added

---
+++ src/unsw/venues/Room.java
@@ -0,0 +1,37 @@
+package unsw.venues;
+
+public class Room {
+    private String name;
+    private String size;
+    private Venue venue;
+
+    public Room(String name, String size, Venue venue) {
+        this.name = name;
+        this.size = size;
+        this.venue = venue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize() {
+        this.size = size;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public getRoom(ArrayList<Room> roomList, String name) {
+        
+    }
+}
